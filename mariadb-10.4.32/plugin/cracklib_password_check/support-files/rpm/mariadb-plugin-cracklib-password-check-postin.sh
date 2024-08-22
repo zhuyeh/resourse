38c5df5df97d7fb7db7de49973e330a39433a775
yhzhu <yhzhu@grandstream.com>
2024-08-22 12:06:23 +0800

    cache maridb10.0

--- mariadb-10.4.32/plugin/cracklib_password_check/support-files/rpm/mariadb-plugin-cracklib-password-check-postin.sh
+++
@@ -1,8 +0,0 @@
-SETARGETDIR=/etc/selinux/targeted/src/policy
-SEDOMPROG=$SETARGETDIR/domains/program
-SECONPROG=$SETARGETDIR/file_contexts/program
-
-if [ -x /usr/sbin/semodule ] ; then
-  /usr/sbin/semodule -i /usr/share/mysql/policy/selinux/mariadb-plugin-cracklib-password-check.pp
-fi
-
--- mariadb-10.4.32/wsrep-lib/wsrep-API/v26/README.md
+++
@@ -1,7 +0,0 @@
-# Write Set Replication API specification
-
-Building:
-```
-cmake [-DCMAKE_BUILD_TYPE=Debug|Release] . && make [VERBOSE=1]
-```
-in top directory.
